64dd381c12a9038f619cbe241e39dbdcf5199d5d
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-01-13 16:14:48 +0000

    Initial restructure due to addition of Launcher submodule.

---
+++ include/maidsafe/directory_info.h
@@ -0,0 +1,82 @@
+/*  Copyright 2015 MaidSafe.net limited
+
+    This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,
+    version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+    licence you accepted on initial access to the Software (the "Licences").
+
+    By contributing code to the MaidSafe Software, or to this project generally, you agree to be
+    bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root
+    directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also
+    available at: http://www.maidsafe.net/licenses
+
+    Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed
+    under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.
+
+    See the Licences for the specific language governing permissions and limitations relating to
+    use of the MaidSafe Software.                                                                 */
+
+#ifndef MAIDSAFE_DIRECTORY_INFO_H_
+#define MAIDSAFE_DIRECTORY_INFO_H_
+
+#include <string>
+
+#include "boost/filesystem/path.hpp"
+
+#include "maidsafe/drive/config.h"
+
+namespace maidsafe {
+
+
+struct DirectoryInfo {
+  enum class AccessRights { kReadOnly, kReadWrite };
+
+  DirectoryInfo() = default;
+
+  DirectoryInfo(const DirectoryInfo&) = default;
+
+  DirectoryInfo(DirectoryInfo&& other)
+      : path(std::move(other.path)),
+        parent_id(std::move(other.parent_id)),
+        directory_id(std::move(other.directory_id)),
+        access_rights(std::move(other.access_rights)) {}
+
+  DirectoryInfo& operator=(const DirectoryInfo&) = default;
+
+  DirectoryInfo& operator=(DirectoryInfo&& other) {
+    path = std::move(other.path);
+    parent_id = std::move(other.parent_id);
+    directory_id = std::move(other.directory_id);
+    access_rights = std::move(other.access_rights);
+    return *this;
+  }
+
+  DirectoryInfo(boost::filesystem::path path_in, drive::ParentId parent_id_in,
+                drive::DirectoryId directory_id_in, AccessRights access_rights_in)
+      : path(std::move(path_in)),
+        parent_id(std::move(parent_id_in)),
+        directory_id(std::move(directory_id_in)),
+        access_rights(std::move(access_rights_in)) {}
+
+  template <typename Archive>
+  Archive& save(Archive& archive) const {
+    return archive(path.string(), parent_id, directory_id, access_rights);
+  }
+
+  template <typename Archive>
+  Archive& load(Archive& archive) {
+    std::string path_as_string;
+    archive(path_as_string, parent_id, directory_id, access_rights);
+    path.assign(std::begin(path_as_string), std::end(path_as_string));
+    return archive;
+  }
+
+  boost::filesystem::path path;
+  drive::ParentId parent_id;
+  drive::DirectoryId directory_id;
+  AccessRights access_rights;
+};
+
+}  // namespace maidsafe
+
+#endif  // MAIDSAFE_DIRECTORY_INFO_H_
---
+++ src/maidsafe/tests/directory_info_test.cc
@@ -0,0 +1,78 @@
+/*  Copyright 2015 MaidSafe.net limited
+
+    This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,
+    version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+    licence you accepted on initial access to the Software (the "Licences").
+
+    By contributing code to the MaidSafe Software, or to this project generally, you agree to be
+    bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root
+    directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also
+    available at: http://www.maidsafe.net/licenses
+
+    Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed
+    under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.
+
+    See the Licences for the specific language governing permissions and limitations relating to
+    use of the MaidSafe Software.                                                                 */
+
+#include "maidsafe/directory_info.h"
+
+#include "maidsafe/common/test.h"
+#include "maidsafe/common/utils.h"
+#include "maidsafe/common/serialisation/serialisation.h"
+
+namespace maidsafe {
+
+namespace test {
+
+testing::AssertionResult Matches(const DirectoryInfo& expected, const DirectoryInfo& actual) {
+  return (expected.path == actual.path && expected.parent_id == actual.parent_id &&
+          expected.directory_id == actual.directory_id &&
+          expected.access_rights == actual.access_rights)
+             ? testing::AssertionSuccess()
+             : testing::AssertionFailure();
+}
+
+TEST(DirectoryInfoTest, BEH_ConstructAndAssign) {
+  // Default c'tor
+  ASSERT_NO_THROW(DirectoryInfo());
+  const DirectoryInfo directory_info;
+  EXPECT_TRUE(directory_info.path.empty());
+  EXPECT_FALSE(directory_info.parent_id->IsInitialised());
+  EXPECT_FALSE(directory_info.directory_id.IsInitialised());
+
+  // C'tor taking value
+  const DirectoryInfo directory_info1(
+      RandomAlphaNumericString(100), drive::ParentId{Identity{RandomAlphaNumericString(64)}},
+      Identity{RandomAlphaNumericString(64)}, DirectoryInfo::AccessRights::kReadOnly);
+  const DirectoryInfo directory_info2(
+      RandomAlphaNumericString(100), drive::ParentId{Identity{RandomAlphaNumericString(64)}},
+      Identity{RandomAlphaNumericString(64)}, DirectoryInfo::AccessRights::kReadWrite);
+
+  // Copy and move
+  DirectoryInfo copied(directory_info1);
+  EXPECT_TRUE(Matches(directory_info1, copied));
+
+  DirectoryInfo moved(std::move(copied));
+  EXPECT_TRUE(Matches(directory_info1, moved));
+
+  copied = directory_info2;
+  EXPECT_TRUE(Matches(directory_info2, copied));
+
+  moved = std::move(copied);
+  EXPECT_TRUE(Matches(directory_info2, moved));
+}
+
+TEST(DirectoryInfoTest, BEH_Serialisation) {
+  const DirectoryInfo directory_info(
+      RandomAlphaNumericString(100), drive::ParentId{Identity{RandomAlphaNumericString(64)}},
+      Identity{RandomAlphaNumericString(64)}, DirectoryInfo::AccessRights::kReadOnly);
+  auto serialised = Serialise(directory_info);
+  auto parsed = Parse<DirectoryInfo>(serialised);
+  EXPECT_TRUE(Matches(directory_info, parsed));
+}
+
+}  // namespace test
+
+}  // namespace maidsafe
